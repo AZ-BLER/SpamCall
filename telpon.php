bc02af9f40dee3beada8a32b1df55aa001d619df
Azbler <55436401+AZ-BLER@users.noreply.github.com>
2019-09-21 01:27:39 +0700

    Update telpon.php

--- telpon.php
+++ telpon.php
@@ -11,9 +11,29 @@ function send($phone){
         curl_close($ch);
                 echo $asw."\n";
 }
-echo "\e[1;92mCreated by ; Azbler\n\n";
-echo "Team ; Bler Squad\n\n";
-echo " Supported ; Dhanbler & Afikbler\n\n";
+import os,sys,time,requests
+GL = "\033[96;1m" # Blue aqua
+BB = "\033[34;1m" # Blue light
+YY = "\033[33;1m" # Yellow light
+GG = "\033[32;1m" # Green light
+WW = "\033[0;1m"  # White light
+RR = "\033[31;1m" # Red light
+CC = "\033[36;1m" # Cyan light
+B = "\033[34m"    # Blue
+Y = "\033[33;1m"    # Yellow
+G = "\033[32m"    # Green
+W = "\033[0;1m"     # White
+R = "\033[31m"    # Red
+C = "\033[36;1m"    # Cyan
+logo = GG+"""
+============================================
+|          S P A M  C A L L                |
+|                                          |
+| Author : Azbler                          |
+| Team   : Bler Squad                      |
+| Contact: 0896-6439-7979                  |
+| Thanks to : Jimi MiSetya                 |
+============================================"""
 echo "NOTE! Limit 3× telpon setiap 1 nomor\n\n";
 echo "Nomor target\nMasukin nomornya (ex;08xxx) : ";
 $nomor = trim(fgets(STDIN));
